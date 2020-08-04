62391c1dac5e29e46ffd5dd7bad0795ed500fee0
Flavio Menezes <flaviomnz@MacBook-Air-de-Flavio.local>
2020-08-04 14:36:15 -0300

    Commit Final Aula 2

--- src/index.js
+++ src/index.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
+
+function Pagina404(){
+  return (
+    <div>
+      Página 404
+    </div>
+  );
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+
+  <Switch>
+    <Route path="/" component={Home} exact />
+    <Route path="/cadastro/video" component={CadastroVideo} />
+    <Route path="/cadastro/categoria" component={CadastroCategoria} />
+    {/* <Route component={ () => (<div>404</div>)} />  SO UM TESTE BOY*/}
+    <Route component={Pagina404} /> {/*Se apagar o path o ultimo sera carregado se nao achar rota*/}
+  </Switch>
+
+  </BrowserRouter>,
+
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
   document.getElementById('root')
 );
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Menu from './components/Menu';
-import dadosIniciais from './data/dados_iniciais.json';
-import BannerMain from './components/BannerMain';
-import Caurosel from './components/Carousel';
-import Footer from './components/Footer';
+import Menu from '../../components/Menu';
+import dadosIniciais from '../../data/dados_iniciais.json';
+import BannerMain from '../../components/BannerMain/';
+import Caurosel from '../../components/Carousel';
+import Footer from '../../components/Footer';
 
 
-function App() {
+function Home() {
   return (
     <div style={ {background:"#141414"} }>
       <Menu />
@@ -23,27 +23,22 @@ function App() {
       />
 
       <Caurosel
-        ignoreFirstVideo
         category={dadosIniciais.categorias[1]}
       />
 
       <Caurosel
-        ignoreFirstVideo
         category={dadosIniciais.categorias[2]}
       />
 
       <Caurosel
-        ignoreFirstVideo
         category={dadosIniciais.categorias[3]}
       />
 
       <Caurosel
-        ignoreFirstVideo
         category={dadosIniciais.categorias[4]}
       />
 
       <Caurosel
-        ignoreFirstVideo
         category={dadosIniciais.categorias[5]}
       />
 
@@ -53,4 +48,4 @@ function App() {
   );
 }
 
-export default App;
+export default Home;

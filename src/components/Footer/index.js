ed3c28fa79ef17b6213a802cc1ff49e659a36c8a
Flavio Menezes <flaviomnz@MacBook-Air-de-Flavio.local>
2020-08-04 15:57:27 -0300

    Final da Aula_3

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { FooterBase } from './styles';
+import { Link } from 'react-router-dom';
+import Logo from '../../assets/img/Logo.png';
 
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
-      </a>
+      <Link to="/">
+            <img className="Logo" src={Logo} alt="MarombaFlix Logo"/ >
+      </Link>
       <p>
-        Orgulhosamente criado durante a
+        Orgulhosamente criado por <a href="https://www.linkedin.com/in/flavio-s-menezes/">
+          Flávio Souto Menezes
+        </a>
         {' '}
+      </p>
+      <p>
         <a href="https://www.alura.com.br/">
           Imersão React da Alura
         </a>
